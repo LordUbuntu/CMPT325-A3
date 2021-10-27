de14710d0668e7bae78401f9fae0191a9c87ccdc
Jacobus Burger <therealjacoburger@gmail.com>
2021-10-27 10:58:49 -0700

    changed port number to a well-known http port (port 8080)

--- server.py
+++ server.py
@@ -7,25 +7,25 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepare a sever socket
 
 # Fill in start
-serverSocket.bind(('127.0.0.1',8810))
-#this gives us only 1 possible connection, hence '1'
+serverSocket.bind(("127.0.0.1", 8080))
+# this gives us only 1 possible connection, hence '1'
 serverSocket.listen(1)
 while True:
     # Establish the connection
-    connectionSocket, addr =  serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept()
     try:
-        #recieving file in packets of size 1024
-        message =  connectionSocket.recv(1024)
+        # recieving file in packets of size 1024
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        #basic file work
-        fileData =  f.read()
+        # basic file work
+        fileData = f.read()
         print(fileData)
-        f.close()# Fill in start #Fill in end
+        f.close()  # Fill in start #Fill in end
         # Send one HTTP header line into socket
-        #OK header
-        connectionSocket.send('HTTP/1.0 200 OK\r\n')
-        #once we have read the file and closed it we can 
+        # OK header
+        connectionSocket.send("HTTP/1.0 200 OK\r\n")
+        # once we have read the file and closed it we can
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
